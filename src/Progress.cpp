efa36bfcceefe88daf6b8e181f62d35e491f429a
Xavier Robin <xavalias-github@xavier.robin.name>
2014-06-26 15:20:36 +0200

    Ignore if testData is an empty matrix

--- src/Progress.cpp
+++ src/Progress.cpp
@@ -5,5 +5,5 @@ using std::string;
 #include "RBM.h"
 
 
-void AcceleratePretrainProgress::propagateData(const RBM& anRBM) {testData = anRBM.predict(testData);}
-void EachStepPretrainProgress::propagateData(const RBM& anRBM) {testData = anRBM.predict(testData);}
+void AcceleratePretrainProgress::propagateData(const RBM& anRBM) {if (testData.size() > 0) testData = anRBM.predict(testData);} // do nothing if no data...
+void EachStepPretrainProgress::propagateData(const RBM& anRBM) {if (testData.size() > 0) testData = anRBM.predict(testData);}
