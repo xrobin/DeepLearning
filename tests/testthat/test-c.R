bc9b62aad336e3723d2bb655394c8260cc39c3e9
Xavier Robin <xavalias-github@xavier.robin.name>
2017-02-28 12:11:00 +0100

    Remove dummy stuff

--- tests/testthat/test-c.R
+++ tests/testthat/test-c.R
@@ -9,20 +9,5 @@ test_that("concatenation works for objects of the same type", {
 	l4b <- Layer(4, "binary")
 	l4c <- Layer(4, "continuous")
 	
-	l3 <- Layer 
-	expect_that(x, throws_error(y))
-	
-	
-	base <- as.POSIXct("2009-08-03 12:01:59.23", tz = "UTC")
-	
-	is_time <- function(x) equals(as.POSIXct(x, tz = "UTC"))
-	floor_base <- function(unit) floor_date(base, unit)
-	
-	expect_that(floor_base("second"), is_time("2009-08-03 12:01:59"))
-	expect_that(floor_base("minute"), is_time("2009-08-03 12:01:00"))
-	expect_that(floor_base("hour"),   is_time("2009-08-03 12:00:00"))
-	expect_that(floor_base("day"),    is_time("2009-08-03 00:00:00"))
-	expect_that(floor_base("week"),   is_time("2009-08-02 00:00:00"))
-	expect_that(floor_base("month"),  is_time("2009-08-01 00:00:00"))
-	expect_that(floor_base("year"),   is_time("2009-01-01 00:00:00"))
+	#l3 <- Layer(3)
 })
