9587bda3d32e24136a7df023b7278b4672f59c4b
Xavier Robin <xavalias-github@xavier.robin.name>
2017-02-28 22:24:34 +0100

    Document the MNIST DBNs

---
+++ R/mnist-data.R
@@ -0,0 +1,9 @@
+#' Pre-trained and fine-tuned DeepBeliefNets for the MNIST dataset
+#' @description These DBNs have already been pre-trained and fine-tuned to model the MNIST dataset. 
+#' They can be used to avoid long training steps, especially in examples of the package documentation.
+#'
+#' @name mnist
+#' @aliases pretrained.mnist trained.mnist
+#' @docType data
+#' @keywords data
+NULL
