d2ee48ce21de6fa0e8bf7322a6dbfc161dad44de
Xavier Robin <xavalias-github@xavier.robin.name>
2017-03-06 12:02:57 +0100

    Create PNGs afterwards

--- tests/video.R
+++ tests/video.R
@@ -32,6 +32,37 @@ if (0) {
 	save(rbm, file = sprintf("video/rbm-4-final.RData", i))
 	dbn[[4]] <- rbm
 	
+	# Fine-tune
+	maxiters.train <- 10000
+	sprintf.fmt.iter <- sprintf("%%0%dd", nchar(sprintf("%d", maxiters.train)))
+	diag <- list(rate = "each", data = NULL, f = function(dbn, batch, data, iter, batchsize, maxiters, layer) {
+		save(dbn, file = sprintf("video/dbn-finetune-%s.RData", sprintf(sprintf.fmt.iter, iter)))
+	})
+	dbn <- train(dbn, mnist$train$x, batchsize = 100, maxiters=maxiters.train,
+					continue.function = continue.function.always, diag = diag)
+	save(dbn, file = sprintf("video/dbn-finetune-final.RData", i))
+	
 }
 
-
+if (0) {
+	library(DeepLearning)
+	library(mnist)
+	data(mnist)
+	
+	dbn <- DeepBeliefNet(Layers(c(784, 1000, 500, 250, 2), input = "continuous", output = "binary"))
+	for (i in 1:3) {
+		load(sprintf("video/rbm-%d-final.RData", i))
+		dbn[[i]] <- rbm
+	}
+	
+	for (file in list.files("video", pattern = "rbm-4-\\d+\\.RData", full.names = TRUE)) {
+		load(file)
+		dbn[[4]] <- rbm
+		
+		png(sub(".RData", ".png", file), width = 1920, height = 1080) # hd output
+		predictions <- predict(dbn, mnist$test$x)
+		reconstructions <- reconstruct(dbn, mnist$test$x)
+		plot.mnist(model = dbn, x = mnist$test$x, label = mnist$test$y+1, predictions = predictions, reconstructions = reconstructions			   )
+		dev.off()
+	}
+}
